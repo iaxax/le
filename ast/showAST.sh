4c3d3a227cc987b264cdc446eeeabd20e67f8f02
anson <1483809252@qq.com>
2017-11-20 20:35:48 +0800

    docs: README

--- ast/showAST.sh
+++ ast/showAST.sh
@@ -1,4 +1,4 @@
-~/Code/Github/rose_build_dir/bin/dotGeneratorWholeASTGraph -c test.c
+ROSE_HOME=/home/anson/Code/Github/rose_build_dir
+
+${ROSE_HOME}/bin/dotGeneratorWholeASTGraph -c test.c
 xdot test.c_AST.dot &
-
-
