fffc4f910e851671eabdbc7aad368b5af40e08c0
anson <1483809252@qq.com>
2017-11-21 00:16:28 +0800

    comment: add comment for message.h

--- include/message.h
+++ include/message.h
@@ -7,12 +7,15 @@
 
 namespace LE {
 
+  // output message to user
   class Message {
   public:
+    // output warning
     static inline void warning(const std::string& message) {
       std::cout << "LOOP EXTRACTION WARNING: " << message << std::endl;
     }
 
+    // output error, process will be terminated
     static inline void error(const std::string& message) {
       std::cout << "LOOP EXTRACTION ERROR: " << message << std::endl;
       std::exit(1);
